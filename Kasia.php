f206e421a3d0a4ed1b1bea16c698170eae20057d
Michcioperz <michcioperz@gmail.com>
2013-08-20 19:53:33 +0000

    Added some content for testing

---
+++ Kasia.php
@@ -0,0 +1,8 @@
+<?php
+define("TEHPAGETITLE", "Kasia");
+require "inc.php";
+mpi_header();
+mpi_poembutton("prawdaoludziach", "Prawda o ludziach");
+mpi_poemmodal("prawdaoludziach", "Prawda o ludziach");
+mpi_footer();
+?>
--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 define("TEHPAGETITLE","Start");
 require "inc.php";
 mpi_header();
+mpi_jumbo("introintro");
 mpi_footer();
 ?>
