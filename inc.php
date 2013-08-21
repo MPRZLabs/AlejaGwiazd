a3ee780be32a483498080b274d3f7500e9993346
Michcioperz <michcioperz@gmail.com>
2013-08-21 21:56:10 +0000

    Flatification

--- inc.php
+++ inc.php
@@ -9,7 +9,6 @@
     echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
     echo "<title>" . TEHPAGETITLE . " - " . TEHSITETITLE . "</title>";
     echo "<link rel=\"stylesheet\" href=\"//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css\">";
-    echo "<link rel=\"stylesheet\" href=\"//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css\">";
     echo "<script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script>";
     echo "<script src=\"//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js\"></script>";
     echo "<link rel=\"stylesheet\" href=\"/css/tehmichiglobalset.css\">";
