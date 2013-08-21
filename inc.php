54f4983ff5f2212d9d6281b753a5634d9bc76f5d
Michcioperz <michcioperz@gmail.com>
2013-08-21 15:10:00 +0000

    fixed carousel titles

--- inc.php
+++ inc.php
@@ -117,9 +117,9 @@
   {
     if ($active)
     {
-      return "<div class=\"item active\"><img src=\"assets/".$person."-carousel.png\" alt=\"".$person."\"><div class=\"carousel-caption\">".$description."</div></div>";
+      return "<div class=\"item active\"><img src=\"assets/".$person."-carousel.png\" alt=\"".$person."\"><div class=\"carousel-caption\"><h3>".$person."</h3><p>".$description."</p></div></div>";
     } else {
-      return "<div class=\"item\"><img src=\"assets/".$person."-carousel.png\" alt=\"".$person."\"><div class=\"carousel-caption\">".$description."</div></div>";
+      return "<div class=\"item\"><img src=\"assets/".$person."-carousel.png\" alt=\"".$person."\"><div class=\"carousel-caption\"><h3>".$person."</h3><p>".$description."</p></div></div>";
     }
   }
 ?>
