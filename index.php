0b118b8bec3b6b6e07758c03a93628add68f35ca
Michcioperz <michcioperz@gmail.com>
2013-08-21 15:01:01 +0000

    moved carousel out of container

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 define("TEHPAGETITLE","Start");
 require "inc.php";
 mpi_header();
-echo mpi_container(mpi_carousel("main-showcase", array("Kasia" => "Rysuję właściwie od zawsze, a od kilku lat również piszę – wiersze, prozę, do wyboru!", "Michcioperz" => "Tutaj kiedyś będzie intro :P")) . mpi_jumbo(mpi_readtextasset("introintro")));
+echo mpi_carousel("main-showcase", array("Kasia" => "Rysuję właściwie od zawsze, a od kilku lat również piszę – wiersze, prozę, do wyboru!", "Michcioperz" => "Tutaj kiedyś będzie intro :P"));
+echo mpi_container(mpi_jumbo(mpi_readtextasset("introintro")));
 mpi_footer();
 ?>
