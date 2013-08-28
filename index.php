4a3063b7940b4b6f1d592fb1ee366ac857983c69
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2013-08-28 20:38:42 +0000

    Typo fixed, world saved. I shall not be spelled lowercase

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require "inc.php";
 mpi_header();
 echo mpi_carousel("main-showcase", array("Kasia" => "Rysuję właściwie od zawsze, a od kilku lat również piszę – wiersze, prozę, do wyboru!", "Michcioperz" => "Tutaj kiedyś będzie intro :P"));
 echo mpi_container(mpi_jumbo(mpi_readtextasset("introintro")));
-echo mpi_showcase_begin().mpi_showcase_person("Kasia").mpi_showcase_person("michcioperz").mpi_showcase_end();
+echo mpi_showcase_begin().mpi_showcase_person("Kasia").mpi_showcase_person("Michcioperz").mpi_showcase_end();
 mpi_footer();
 ?>
