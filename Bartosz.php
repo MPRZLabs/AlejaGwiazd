16e5f0c2eef9cfdfa0a775411f2e0a28712c6cf3
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2013-09-01 21:18:37 +0000

    Added two people

---
+++ Bartosz.php
@@ -0,0 +1,8 @@
+<?php
+define("TEHPAGETITLE", "Bartosz");
+require "inc.php";
+mpi_header();
+echo mpi_carousel("Bartosz", array("Bartosz" => "w programie Oddawaj Fartucha <small>czyli epicki pojedynek Mordziak vs. Pan Pstrąg</small><br /><p class=\"lead\">Bartosz - chluba klasy IIa, po mieczu i po kądzieli pierwszorzędny restaurator, urodzony kucharz i piekarz, największy smakosz Rzeczypospolitej. Robi rewelacyjne ciasta, którymi raczy swoją klasę. I choć nie zawsze zapamięta skomplikowane wzory matematyczne czy fizyczne, przepisu na pstrąga nie zapomni nigdy.</p>"));
+mpi_jumbovid("popcorn", "iwt");
+mpi_footer();
+?>
---
+++ GAndzia.php
@@ -0,0 +1,8 @@
+<?php
+define("TEHPAGETITLE", "GAndzia");
+require "inc.php";
+mpi_header();
+echo mpi_carousel("GAndzia", array("GAndzia" => ""));
+echo mpi_container(mpi_showcase_g("GAndzia", mpi_showcase_image("GAndzia", "custom/201301/portret2-538a33").mpi_showcase_image("GAndzia", "custom/201301/portret3-1a159f").mpi_showcase_image("GAndzia", "custom/201301/4-74f26a").mpi_showcase_image("GAndzia", "custom/201301/5-1a26e9").mpi_showcase_image("GAndzia", "custom/201301/6-790661").mpi_showcase_image("GAndzia", "custom/201301/7-8d80ee").mpi_showcase_image("GAndzia", "custom/201301/portret-5cd149")));
+mpi_footer();
+?>
