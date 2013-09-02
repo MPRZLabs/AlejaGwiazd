c72abc7d26878a2dd22ce58808588a2c0d57a583
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2013-09-02 13:29:16 +0000

    stuff looks better with indentation (that's how I spell it?)

--- Gabi.php
+++ Gabi.php
@@ -3,7 +3,36 @@ define("TEHPAGETITLE", "Gabi");
 require "inc.php";
 mpi_header();
 echo mpi_carousel("Gabi", array("Gabi" => "<center><img class=\"img-responsive\" style=\"max-height:200px;\" src=\"assets/Gabi-avatar.png\" /></center><br />Wysoka, powyżej normy, chce się wyróżniać, ale i tak jest mainstream'owa. Podobno wygląda na wyniosłą i apodyktyczną, jednak jest bardzo gadatliwa i przyjazna otoczeniu.  Ma psychologiczne zapędy. Interesuje ją każdy rodzaj sztuki. Zawsze nie ma czasu, wiecznie odnajduje nowe rozwiązania i wymyśla zmiany w swoim pokoju. W wolnych chwilach nieumiejętnie kucharzy, rozwija nowe umiejętności (ostatnio uczy się szyć)."));
-echo mpi_container(mpi_showcase_g("Gabi1", mpi_showcase_image("Gabi1", "custom/201302/202806_255855997850738_838042807_o-1b9e89").mpi_showcase_image("Gabi1", "custom/201301/DSC00749-4c4f6a").mpi_showcase_image("Gabi1", "custom/201105/DSC03182-192149").mpi_showcase_image("Gabi1", "custom/201105/DSC03100-6ec517").mpi_showcase_image("Gabi1", "custom/201105/DSC03026-6cc1ce").mpi_showcase_image("Gabi1", "custom/201104/DSC02050-375346").mpi_showcase_image("Gabi1", "custom/201104/DSC02025-593d4b").mpi_showcase_image("Gabi1", "custom/201104/DSC01984-2f0159").mpi_showcase_image("Gabi1", "custom/201104/DSC01907-7df165").mpi_showcase_image("Gabi1", "custom/201104/DSC01918-12d590").mpi_showcase_image("Gabi1", "custom/201104/DSC01921-1fd0ae").mpi_showcase_image("Gabi1", "custom/201103/DSC01599-4cf848").mpi_showcase_image("Gabi1", "custom/201103/DSC01556-654052").mpi_showcase_image("Gabi1", "custom/201103/DSC01566-8f5c87").mpi_showcase_image("Gabi1", "custom/201103/DSC01579-3eeeda")));
-echo mpi_container(mpi_showcase_g("Gabi2", mpi_showcase_image("Gabi2", "custom/201302/DSC01675-8fc3ae").mpi_showcase_image("Gabi2", "custom/201302/DSC01724-1c10bd").mpi_showcase_image("Gabi2", "custom/201302/DSC01745-30dfcb").mpi_showcase_image("Gabi2", "custom/201302/DSC01522-42e313").mpi_showcase_image("Gabi2", "custom/201302/DSC01406-fa6cb").mpi_showcase_image("Gabi2", "custom/201302/DSC01430-244421").mpi_showcase_image("Gabi2", "custom/201302/DSC01431-8e57e9").mpi_showcase_image("Gabi2", "custom/201302/DSC01437-6e023b").mpi_showcase_image("Gabi2", "custom/201302/DSC01457-f465b").mpi_showcase_image("Gabi2", "custom/201302/DSC01460-47dd4d").mpi_showcase_image("Gabi2", "custom/201302/DSC01464-2bc9de").mpi_showcase_image("Gabi2", "custom/201302/DSC01469-9352c5")));
+echo mpi_container(mpi_showcase_g("Gabi1",
+  mpi_showcase_image("Gabi1", "custom/201302/202806_255855997850738_838042807_o-1b9e89").
+  mpi_showcase_image("Gabi1", "custom/201301/DSC00749-4c4f6a").
+  mpi_showcase_image("Gabi1", "custom/201105/DSC03182-192149").
+  mpi_showcase_image("Gabi1", "custom/201105/DSC03100-6ec517").
+  mpi_showcase_image("Gabi1", "custom/201105/DSC03026-6cc1ce").
+  mpi_showcase_image("Gabi1", "custom/201104/DSC02050-375346").
+  mpi_showcase_image("Gabi1", "custom/201104/DSC02025-593d4b").
+  mpi_showcase_image("Gabi1", "custom/201104/DSC01984-2f0159").
+  mpi_showcase_image("Gabi1", "custom/201104/DSC01907-7df165").
+  mpi_showcase_image("Gabi1", "custom/201104/DSC01918-12d590").
+  mpi_showcase_image("Gabi1", "custom/201104/DSC01921-1fd0ae").
+  mpi_showcase_image("Gabi1", "custom/201103/DSC01599-4cf848").
+  mpi_showcase_image("Gabi1", "custom/201103/DSC01556-654052").
+  mpi_showcase_image("Gabi1", "custom/201103/DSC01566-8f5c87").
+  mpi_showcase_image("Gabi1", "custom/201103/DSC01579-3eeeda")
+));
+echo mpi_container(mpi_showcase_g("Gabi2",
+  mpi_showcase_image("Gabi2", "custom/201302/DSC01675-8fc3ae").
+  mpi_showcase_image("Gabi2", "custom/201302/DSC01724-1c10bd").
+  mpi_showcase_image("Gabi2", "custom/201302/DSC01745-30dfcb").
+  mpi_showcase_image("Gabi2", "custom/201302/DSC01522-42e313").
+  mpi_showcase_image("Gabi2", "custom/201302/DSC01406-fa6cb").
+  mpi_showcase_image("Gabi2", "custom/201302/DSC01430-244421").
+  mpi_showcase_image("Gabi2", "custom/201302/DSC01431-8e57e9").
+  mpi_showcase_image("Gabi2", "custom/201302/DSC01437-6e023b").
+  mpi_showcase_image("Gabi2", "custom/201302/DSC01457-f465b").
+  mpi_showcase_image("Gabi2", "custom/201302/DSC01460-47dd4d").
+  mpi_showcase_image("Gabi2", "custom/201302/DSC01464-2bc9de").
+  mpi_showcase_image("Gabi2", "custom/201302/DSC01469-9352c5")
+));
 mpi_footer();
 ?>
