314e7d34cb943e82e20612e1378b503390652d14
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2013-10-09 21:03:39 +0000

    fixed Innerv's Facebook like button

--- Innervision.php
+++ Innervision.php
@@ -3,7 +3,7 @@ define("TEHPAGETITLE", "Innervision");
 define("MPI_FBSDK", true);
 require "inc.php";
 mpi_header();
-echo mpi_carousel("Innervision", array("Innervision" => mpi_facebox("https://www.facebook.com/pages/Innervision/119974921544849")));
+echo mpi_carousel("Innervision", array("Innervision" => mpi_facebox("http://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FInnervision%2F119974921544849&height=80&colorscheme=light&layout=standard&action=like&show_faces=true&send=false")));
 mpi_jumbovid("facebook", "https://www.facebook.com/photo.php?v=435051989926958");
 mpi_jumbovid("facebook", "https://www.facebook.com/photo.php?v=435054046593419");
 mpi_jumbovid("facebook", "https://www.facebook.com/photo.php?v=435056003259890");
