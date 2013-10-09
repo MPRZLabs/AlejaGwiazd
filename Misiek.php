5501449dafbf649f68a7376aaa105e324664fc6a
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2013-10-09 20:28:01 +0000

    he had bad formatting

--- Misiek.php
+++ Misiek.php
@@ -2,7 +2,7 @@
 define("TEHPAGETITLE", "Misiek");
 require "inc.php";
 mpi_header();
-echo mpi_carousel("Misiek", array("Misiek" => "<img src=\"assets/Misiek-avatar.png\" style=\"max-height: 200px;\" class=\"thumbnail pull-left\" /><p class=\"lead\">genialny kaskader, obecnie marzy o różowym rowerku</p><br /><iframe src=\"//www.facebook.com/plugins/like.php?locale=pl_PL&href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FMisiek-Stunt-Streetbike-Freestyle%2F164263133767090&amp;width=The+pixel+width+of+the+plugin&amp;height=80&amp;colorscheme=light&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;send=true\" scrolling=\"no\" frameborder=\"0\" style=\"border:none; overflow:hidden; height:160px;\" allowTransparency=\"true\"></iframe>"));
+echo mpi_carousel("Misiek", array("Misiek" => "<center><img class=\"img-responsive\" style=\"max-height:200px;\" src=\"assets/Misiek-avatar.png\" /></center><br />genialny kaskader, obecnie marzy o różowym rowerku<br /><iframe src=\"//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FMisiek-Stunt-Streetbike-Freestyle%2F164263133767090&amp;width=The+pixel+width+of+the+plugin&amp;height=80&amp;colorscheme=light&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;send=true\" scrolling=\"no\" frameborder=\"0\" style=\"border:none; overflow:hidden; height:160px;\" allowTransparency=\"true\"></iframe>"));
 mpi_jumbovid("yt", "CCD2ZHn3gzU");
 mpi_footer();
 ?>
