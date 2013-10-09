b9edbe02151e84db19d8e54a68e464c0e0f13767
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2013-10-09 20:13:42 +0000

    adding Misiek, hope it works

---
+++ Misiek.php
@@ -0,0 +1,8 @@
+<?php
+define("TEHPAGETITLE", "Misiek");
+require "inc.php";
+mpi_header();
+echo mpi_carousel("Misiek", array("Misiek" => "genialny kaskader, obecnie marzy o różowym rowerku<br /><iframe src=\"//www.facebook.com/plugins/like.php?locale=pl_PL&href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FMisiek-Stunt-Streetbike-Freestyle%2F164263133767090&amp;width=The+pixel+width+of+the+plugin&amp;height=80&amp;colorscheme=light&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;send=true\" scrolling=\"no\" frameborder=\"0\" style=\"border:none; overflow:hidden; height:160px;\" allowTransparency=\"true\"></iframe>"));
+mpi_jumbovid("yt", "CCD2ZHn3gzU");
+mpi_footer();
+?>
