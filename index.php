d7c7d298079dee0fc233d9c0783a75433a92c7df
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2013-10-14 13:42:42 +0200

    Fixed frontpage

--- index.php
+++ index.php
@@ -4,13 +4,14 @@ require "inc.php";
 mpi_header();
 echo mpi_carousel("main-showcase", array(
   "Bartosz" => "Epicki pojedynek w kuchni - Mordziak vs. Pan Pstrąg",
-  //"Eliza" => "\"No tak jakoś wyszło\"",
+  "Eliza" => "",
   "Gabi" => "wysoka, powyżej normy, chce się wyróżniać, ale i tak jest mainstream'owa. Podobno wygląda na wyniosłą i apodyktyczną, jednak jest bardzo gadatliwa i przyjazna otoczeniu.  Ma psychologiczne zapędy. Interesuje ją każdy rodzaj sztuki.",
   "GAndzia" => "",
   "Innervision" => "Nasze odkrycie - przyszłe gwiazdy rocka/metalu",
   "Kasia" => "Rysuję właściwie od zawsze, a od kilku lat również piszę – wiersze, prozę, do wyboru!",
   "Keram" => "B-boy",
-  "Michcioperz" => "Tutaj kiedyś będzie intro :P"
+  "Michcioperz" => "Tutaj kiedyś będzie intro :P",
+  "Misiek" => "genialny kaskader, marzy o różowym rowerku"
 ));
 echo mpi_container(mpi_jumbo(mpi_readcodeasset("introintro")));
 echo mpi_container(mpi_showcase(
@@ -22,6 +23,7 @@ echo mpi_container(mpi_showcase(
   mpi_showcase_person("Kasia").
   mpi_showcase_person("Keram").
   mpi_showcase_person("Michcioperz")
+  mpi_showcase_person("Misiek");
 ));
 mpi_footer();
 ?>
